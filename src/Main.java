e77446bbd34094b9b4150cc78bb29fa696255269
AizhamalAmanturova <jama.issykkul@gmail.com>
2023-10-08 20:21:58 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.time.DayOfWeek;
+import java.time.Month;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] commands = {"Сидеть", "Лежать"};
+
+        Shelter shelter = new Shelter("Romashka",
+                "Lenina 1");
+
+        Dog dog = new Dog("Rex",
+                "Ovcharka", commands, shelter, ColorEnum.BROWN);
+
+        System.out.println(dog.getInfo());
+        dog.makeVoice();
+        dog.makeVoice("Гув гув");
+        dog.makeVoice("Гив гив");
+
+    }
+}
